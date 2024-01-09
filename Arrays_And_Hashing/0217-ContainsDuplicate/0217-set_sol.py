c7f16e50d872d68be57af9a5a061ae7778bb4930
Donvis Nguyen <donvisnguyen@gmail.com>
2024-01-09 23:33:02 +0800

    docs: rename file

--- Arrays_And_Hashing/0217-ContainsDuplicate/0217-set_sol.py
+++ Arrays_And_Hashing/0217-ContainsDuplicate/0217-set_sol.py
@@ -1,5 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # We are finding for duplicates, so it is a 'frequency' problem.
         # Since sets does not allow any duplicates, we can just compare the length of a set that we put nums into, with the length of the list nums..
         # If the length is equal, it means there are no duplicates, and thus we return false.
         # If the length is not equal, it means that some integers are removed from the set, implying that there are duplicates and thus we return true

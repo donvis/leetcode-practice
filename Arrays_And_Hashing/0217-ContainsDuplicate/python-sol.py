3d4dea7e62492839f4a7bac7f99fd9d20abeb181
Donvis Nguyen <donvisnguyen@gmail.com>
2024-01-09 23:30:54 +0800

    create: python set solution for 0217 Contains Duplicate

---
+++ Arrays_And_Hashing/0217-ContainsDuplicate/python-sol.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # Since sets does not allow any duplicates, we can just compare the length of a set that we put nums into, with the length of the list nums..
+        # If the length is equal, it means there are no duplicates, and thus we return false.
+        # If the length is not equal, it means that some integers are removed from the set, implying that there are duplicates and thus we return true
+        if len(set(nums))==len(nums):
+            return False
+        else: return True

fe8c4288c7cb5f9b35056db114ebeece9d2f82fe
Donvis Nguyen <donvisnguyen@gmail.com>
2024-01-09 23:51:39 +0800

    refactor: add a guard clause

--- Arrays_And_Hashing/0242-ValidAnagram/0242-sort_sol.py
+++ Arrays_And_Hashing/0242-ValidAnagram/0242-sort_sol.py
@@ -3,6 +3,11 @@ class Solution:
         # if s is an anagram of t, they both should have the exact same characters, in the same order after being sorted.
         # By sorting both strings, we check if they are equal.
         # Time complexity should be O(nlogn) due to sorting being used.
+
+
+        # If the length is not equal at all, it is definitely not an anagram
+        if len(s) != len(t):
+            return False
         s =sorted(s)
         t =sorted(t)
         if s == t:
